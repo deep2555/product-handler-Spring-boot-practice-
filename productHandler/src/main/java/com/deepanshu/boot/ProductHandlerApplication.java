67750d712d4e186f8b567fb635a4ce5f106f7652
Deepanshu <deepanshuprashar83@gmail.com>
2026-07-11 15:15:58 +0200

    initial project on 11-07-2026 by deepanshu

---
+++ productHandler/src/main/java/com/deepanshu/boot/ProductHandlerApplication.java
@@ -0,0 +1,14 @@
+package com.deepanshu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductHandlerApplication.class, args);
+		System.out.println("spring boot practice");
+	}
+
+}
---
+++ productHandler/src/test/java/com/deepanshu/boot/ProductHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepanshu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
